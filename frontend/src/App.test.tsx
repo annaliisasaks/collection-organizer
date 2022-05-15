3dbbbbf79e47a9e4dea5dde16350bc2ba800041b
Anna-Liisa Saks <anna-liisa.saks@khk.ee>
2022-05-15 15:51:44 +0300

    Add LogRocket secrets

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './App';
+
+// describe('Test App Entry point', () => {
+//  it('should have login text!', () => {
+//    const wrapper = mount(<App />);
+//    expect(wrapper.find('div.app')).toHaveLength(1);
+//  });
+// });
+
+describe('Fakes test', () => {
+  it('should assert that true is truthy', () => {
+    expect(true).toBeTruthy();
+  });
+});
